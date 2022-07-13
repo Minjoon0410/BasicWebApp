d6d3b0bdf0ce015ed55f10812e24e049da98d38c
Minjoon <ge95tox@mytum.de>
2022-07-13 10:52:38 +0200

    git commit --allow-empty -m "trigger pipeline"

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,7 +13,10 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "Minjoon";
-        } else { // TODO extend the programm here
+        } else if (query.contains("plus")) {
+            return "25";
+        }
+        else { // TODO extend the programm here
             return "";
         }
     }
